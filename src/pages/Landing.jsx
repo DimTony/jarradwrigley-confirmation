4fd06e371765a1f3d56c0b10625679a88bfaa053
Obinna Anthony Dimojiaku <dimojiakuo@accessbankplc.com>
2024-12-12 16:43:10 +0100

    sam archer pdf

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -137,7 +137,10 @@ const Landing = () => {
     <Box p="0.5rem" w="100vw" h="100vh">
       <VStack alignItems="flex-start" w="100%" h="100%" overflow="hidden">
         <Navbar />
-        <a style={{ display: "none" }} href={pdf}>
+        <a
+          // style={{ display: "none" }}
+          href={pdf}
+        >
           downlod
         </a>
         {content}
